4668314f5b6dfeaefddec65b2c0a4c30d2ee002d
diegorezm <dmourarezende@gmail.com>
2025-07-14 00:25:19 -0300

    adding spaces

--- src/app/(public)/(home)/_components/hero.tsx
+++ src/app/(public)/(home)/_components/hero.tsx
@@ -11,10 +11,12 @@ export function Hero() {
           Desenvolvimento de Software
         </Badge>
         <h1 className="text-4xl md:text-6xl lg:text-7xl font-bold text-gray-900 dark:text-white mb-6 leading-tight">
-          Transformamos suas
+          Transformamos suas{" "}
           <span className="bg-gradient-to-r from-purple-500 to-violet-500 bg-clip-text text-transparent">
             ideias
           </span>
+
+          {" "}
           em soluções digitais
         </h1>
         <p className="text-xl text-gray-600 dark:text-gray-300 mb-8 max-w-3xl mx-auto leading-relaxed">
