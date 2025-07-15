6803a9452af0728ba20b1a02911c42bbd13a6806
diegorezm <dmourarezende@gmail.com>
2025-07-15 18:24:49 -0300

    adding SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,12 +6,57 @@ import "./globals.css";
 
 const inter = Inter({
   subsets: ["latin"],
-  weight: ["400", "500", "600", "700", "900"]
+  weight: ["400", "500", "600", "700", "900"],
 });
 
 export const metadata: Metadata = {
   title: "Morzen - Desenvolvimento de Software",
-  description: "Transformamos suas ideias em soluções digitais inovadoras",
+  description:
+    "Transformamos suas ideias em soluções digitais inovadoras, com foco em performance, escalabilidade e experiência do usuário.",
+  keywords: [
+    "Desenvolvimento de software",
+    "Aplicativos web",
+    "Soluções digitais",
+    "Agência de tecnologia",
+    "Desenvolvimento personalizado",
+    "Morzen",
+  ],
+  authors: [{ name: "Morzen", url: "https://morzen.com.br" }],
+  creator: "Morzen",
+  publisher: "Morzen",
+  metadataBase: new URL("https://morzen.com.br"),
+  alternates: {
+    canonical: "https://morzen.com.br",
+  },
+  openGraph: {
+    type: "website",
+    locale: "pt_BR",
+    url: "https://morzen.com.br",
+    title: "Morzen - Desenvolvimento de Software",
+    description:
+      "Transformamos suas ideias em soluções digitais inovadoras, com foco em performance, escalabilidade e experiência do usuário.",
+    siteName: "Morzen",
+    images: [
+      {
+        url: "https://morzen.com.br/og.png",
+        width: 1200,
+        height: 630,
+        alt: "Logo da Morzen com fundo escuro",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Morzen - Desenvolvimento de Software",
+    description:
+      "Transformamos suas ideias em soluções digitais inovadoras, com foco em performance, escalabilidade e experiência do usuário.",
+    images: ["https://morzen.com.br/og.png"],
+    site: "@morzen.zenmor",
+    creator: "@morzen.zenmor",
+  },
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
